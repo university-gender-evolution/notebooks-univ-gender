15b3a4d529811d6b1022655778bcdafe9303d8be
krishnab <cyclotomiq@gmail.com>
2016-07-12 18:52:46 -0700

    updated build script with environment variable.

--- notebooks/build_script/hoffman_notebook_build_script.sh
+++ notebooks/build_script/hoffman_notebook_build_script.sh
@@ -5,13 +5,13 @@ echo "Starting build script"
 
 echo "Lets begin by getting the latest copy of the notebooks."
 
-cd notebooks_gender_univ/notebooks
+cd $HOME/notebooks_gender_univ/notebooks
 
 git pull origin master
 
 echo "Now lets get the latest copy of the python package."
 
-cd notebooks_gender_univ/py-university-gender-dynamics-pkg/
+cd $HOME/notebooks_gender_univ/py-university-gender-dynamics-pkg/
 
 git pull origin master
 
